959da250e445599952d532fd1edc52fa101bc90e
moJobSrc <leekus1005@gmail.com>
2021-07-09 00:03:00 +0900

    code improve

--- app/src/main/java/com/daesin/school/HomeFragment.kt
+++ app/src/main/java/com/daesin/school/HomeFragment.kt
@@ -17,21 +17,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
+import kotlin.math.log
 
 class HomeFragment : Fragment() {
     //공지사항
     private var notice = ArrayList<NotiData>()
     //가정통신문
     private var letter = ArrayList<NotiData>()
-    //칭찬합시다(변경예정이라 만들어지진않음)
-    var praise = ArrayList<NotiData>()
-
-    //공지사항
-    private val noticeUrl = "http://school.busanedu.net/daesin-m/na/ntt/selectNttList.do?mi=618566&bbsId=1011229&listCo=5"
-    //가정통신문
-    private val letterUrl = "http://school.busanedu.net/daesin-m/na/ntt/selectNttList.do?mi=618567&bbsId=1011230&listCo=5"
-    //칭찬합시다
-    private val praiseUrl = "http://school.busanedu.net/daesin-m/main.do"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         parse()
@@ -40,41 +32,46 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //공지사항 클릭
+        //공지사항 클릭 내용변경
         home_alert.setOnClickListener { recyclerView.adapter = NotiAdapater(notice); change(home_alert) }
         //가정통신문 클릭
         letter_alert.setOnClickListener { recyclerView.adapter = NotiAdapater(letter); change(letter_alert) }
-        //칭찬합시다
-        praise_alert.setOnClickListener { recyclerView.adapter = NotiAdapater(praise); change(praise_alert) }
     }
     private fun parse() {
         GlobalScope.launch(Dispatchers.IO) {
-            getList(noticeUrl, notice)
-            getList(letterUrl, letter)
-            //칭찬합시다 불러오는부분 오류나서 주석처리
-//            val jsoup = Jsoup.connect(praiseUrl).get
-//            val doc = jsoup.select(".widgDiv notice1051 > div:nth-child(4)")
-//            Log.d("noti", doc.html())
-//            for (ele in doc) {
-//                praise.add(NotiData(ele.select("a").text()))
-//                Log.d("noti", ele.select("a").text())
-//            }
+            getNotice()
+            getLetter()
+
             activity?.runOnUiThread {
                 recyclerView.layoutManager = LinearLayoutManager(requireContext())
                 recyclerView.adapter = NotiAdapater(notice)
             }
         }
     }
-
-    //파싱url과 리스트넣으면 리스트에 파싱값을 넣어줌
-    private fun getList(url: String, list: ArrayList<NotiData>) {
-        val jsoup = Jsoup.connect(url).get()
+    
+    //공지사항
+    private fun getNotice() {
+        val jsoup = Jsoup.connect("http://school.busanedu.net/daesin-m/na/ntt/selectNttList.do?mi=618566&bbsId=1011229")
+                .data("listCo","5").post()
         val doc = jsoup.select("tbody tr")
-
         for (tr in doc) {
             //공지가아닌것만 가져오기
+
             if (tr.select("td:nth-child(1)").text() != "공지")
-                list.add(NotiData(title = tr.select("td:nth-child(2)").text()))
+                notice.add(NotiData(title = tr.select("td:nth-child(2)").text()))
+        }
+    }
+    
+    //가정통신문
+    private fun getLetter() {
+        val jsoup = Jsoup.connect("http://school.busanedu.net/daesin-m/na/ntt/selectNttList.do?mi=618567&bbsId=1011230")
+                .data("listCo","5").post()
+        val doc = jsoup.select("tbody tr")
+        for (tr in doc) {
+            //공지가아닌것만 가져오기
+
+            if (tr.select("td:nth-child(1)").text() != "공지")
+                letter.add(NotiData(title = tr.select("td:nth-child(2)").text()))
         }
     }
 
@@ -82,7 +79,6 @@ class HomeFragment : Fragment() {
     private fun change(textView: TextView) {
         home_alert.setTextColor(resources.getColor(R.color.grey))
         letter_alert.setTextColor(resources.getColor(R.color.grey))
-        praise_alert.setTextColor(resources.getColor(R.color.grey))
         textView.setTextColor(resources.getColor(R.color.colorPrimaryDark))
     }
 }
